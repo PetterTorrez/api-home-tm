3f3a3cd637623a54289c80c7209a6a407375a149
PetterTorrez <tec.isc.torres@gmail.com>
2026-04-13 20:59:29 -0700

    main files

---
+++ src/main/java/com/example/home_tm/HomeTmApplication.java
@@ -0,0 +1,13 @@
+package com.example.home_tm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeTmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeTmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/home_tm/HomeTmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.home_tm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeTmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
